848fc89e6ae8351443a91744ae7f335a756c8199
Mrinal-K-Mishra <107672054+Mrinal-K-Mishra@users.noreply.github.com>
2022-06-24 13:05:36 +0530

    Add files via upload

---
+++ Cjava.java
@@ -0,0 +1,11 @@
+// @Author Mrinal Kanti Mishra
+
+//Jav Prgm. to print 2 line
+
+class Cjava{
+   public static void main(String[] args){
+    System.out.println("Hello World.");            //Print 1rst line.
+    System.out.println("First Program");           //Print 2nd line.
+}
+
+}
