905194248db282ecc6bfea43f89b04a4f3a40187
Mrinal-K-Mishra <107672054+Mrinal-K-Mishra@users.noreply.github.com>
2022-07-05 11:35:55 +0530

    Update Inheritance.java

--- Inheritance.java
+++ Inheritance.java
@@ -1,10 +1,10 @@
-public class Employee {
+public class Employee {     //parent/base class
 	float salary = 400000;
 }
-class Inherit extends Employee {
+class Inherit extends Employee {    //child/derived/extended
 	int bonus =10000;
 	public static void main(String[] args) {
-		Inherit i = new Inherit();
+		Inherit i = new Inherit();      //obj. or child class
 		System.out.println("Salary is:" + i.salary);
 		System.out.println("Bonus is:" + i.bonus);
 	}
