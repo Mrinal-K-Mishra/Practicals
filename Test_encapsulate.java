06f1980adbd3429f4e4df9fdbcd9080e166ee1d6
Mrinal-K-Mishra <107672054+Mrinal-K-Mishra@users.noreply.github.com>
2022-07-14 11:09:44 +0530

    Update Test_encapsulate.java

--- Test_encapsulate.java
+++ Test_encapsulate.java
@@ -3,7 +3,7 @@ package com;
 public class Test_encapsulate {
 	public static void main(String args[]) {
 		Student_Encapsulate obj = new Student_Encapsulate();
-		obj.setName("Jaita"); //setting the value
+		obj.setName("Mrinal"); //setting the value
 		System.out.println(obj.getName());//getting the value
 	}
 
