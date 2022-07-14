9aec713dc5cfa7fd40565ce16b3869233f43d08a
Mrinal-K-Mishra <107672054+Mrinal-K-Mishra@users.noreply.github.com>
2022-07-14 11:44:26 +0530

    Add files via upload

---
+++ RectangleParameter.java
@@ -0,0 +1,21 @@
+
+public class RectangleParameter {
+	int length;
+	int width;
+	void insert(int l,int w) {
+		length=l;
+		width=w;
+		}
+   void calculateParameter() {
+	   System.out.println(2*(length+width)); 
+	   }
+   
+static class RectangleParameter1{
+	public static void main(String args[]) {
+  RectangleParameter r1=new RectangleParameter();
+  r1.insert(11,5);
+  r1.calculateParameter();
+ 
+	}
+ }
+}
