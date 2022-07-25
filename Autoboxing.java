f5cb762c3d5967f4d710f497561f9efc140eea85
Mrinal-K-Mishra <107672054+Mrinal-K-Mishra@users.noreply.github.com>
2022-07-25 10:54:51 +0530

    Add files via upload

---
+++ Autoboxing.java
@@ -0,0 +1,12 @@
+package Wrapper;
+//primitive to wrapper
+public class Autoboxing {
+	public static void main(String args[]) {
+		int a=10;
+		Integer i = Integer.valueOf(a);//converting int into Integer
+		Integer j =a;// now compiler will write Integer.valueof(a) autoboxing
+		System.out.println(a+" "+i+" "+j);
+	}
+	}
+
+
---
+++ Autoboxingeg.java
@@ -0,0 +1,15 @@
+package Wrapper;
+import java.util.ArrayList;
+//collection framework(ArrayList)
+public class Autoboxingeg {
+	public static void main(String arg[]) {
+		ArrayList<Integer>a1=new ArrayList<Integer>();
+		// creating an empty array list for integer type
+		a1.add(1);// adding the int primitive type valueby using add() method
+		a1.add(2);
+		a1.add(3);
+		System.out.println("ArrayList:" + a1);
+		}
+	}
+
+
