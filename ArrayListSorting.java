004c8d12204e7779ba809ebc28602a2cf407d159
Mrinal-K-Mishra <107672054+Mrinal-K-Mishra@users.noreply.github.com>
2022-08-14 22:27:26 +0530

    Update ArrayListSorting.java

--- ArrayListSorting.java
+++ ArrayListSorting.java
@@ -23,14 +23,14 @@ public class ArrayListSorting {
 			System.out.println();
 			ArrayList<Integer> al2 = new ArrayList<>();
 			// adding Integer object to array list
-			al2.add(42);
-			al2.add(62);
-			al2.add(59);
-			al2.add(98);
-			al2.add(59);
-			al2.add(80);
-			al2.add(78);
-			al2.add(36);
+			al2.add(76);
+			al2.add(89);
+			al2.add(94);
+			al2.add(43);
+			al2.add(32);
+			al2.add(17);
+			al2.add(25);
+			al2.add(51);
 			al2.add(-12);
 			System.out.println("Integer sorted list: ");
 			// sorting array list
